bfaa1c4728f0d07c539a6ce3283abf05ceb67058
YueeLiuu <2261464l@student.gla.ac.uk>
2021-07-30 01:36:48 +0100

    add chapter5 and part of 6

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,9 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#print(__file__)
-#print(os.path.dirname(__file__))
-#print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 
